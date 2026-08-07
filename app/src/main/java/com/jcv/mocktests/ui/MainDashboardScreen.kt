31a435683578437ca82deda30597a6307625c191
NAGARJUNAGALLA <nagarjunagalla@gmail.com>
2026-08-07 15:28:15 +0530

    Update image resource from ic_launcher to logo

--- app/src/main/java/com/jcv/mocktests/ui/MainDashboardScreen.kt
+++ app/src/main/java/com/jcv/mocktests/ui/MainDashboardScreen.kt
@@ -831,7 +831,7 @@ fun UserProfileHeader(auth: FirebaseAuth) {
             verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
-                painter = painterResource(id = R.mipmap.ic_launcher), // Or R.drawable.logo
+                painter = painterResource(id = R.drawable.logo), 
                 contentDescription = "App Logo",
                 modifier = Modifier
                     .size(80.dp)
