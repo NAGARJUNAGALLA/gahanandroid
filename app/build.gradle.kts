1e681a32aff6dcb6240ca60ad6f5eab94aebe2dd
NAGARJUNAGALLA <nagarjunagalla@gmail.com>
2026-08-08 06:36:55 +0530

    Update dependencies in build.gradle.kts
    Removed comments for Jetpack Compose & Navigation and Firebase (Auth & Firestore) dependencies. Added Firebase Analytics dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,6 @@ android {
 }
 
 dependencies {
-    // Jetpack Compose & Navigation
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -66,10 +65,9 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-
-    // Firebase (Auth & Firestore)
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("com.google.firebase:firebase-analytics")
 }
